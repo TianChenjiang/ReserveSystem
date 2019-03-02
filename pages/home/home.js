9fa031569e69b4d7921c25d31d232e763a0d8df6
FNhaha <584948862@qq.com>
2019-03-02 14:42:32 +0800

    临时保存

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,1 +1,28 @@
-Page({})
+Page({
+  data: {
+    value: ''
+  },
+
+  onChange(e) {
+    this.setData({
+      value: e.detail
+    });
+  },
+
+  onSearch(event) {
+    if (this.data.value) {
+      wx.showToast({
+        title: '搜索：' + this.data.value,
+        icon: 'none'
+      });
+    }
+    else{
+      wx.showToast({
+        title: '请输入搜索内容',
+        icon:'none'
+      })
+    }
+  },
+
+  
+});

b815ac32cb469732fc4cda81bc9b7251d8bf1e3b
TianChenjiang <tiancj0310@outlook.com>
2019-03-04 23:59:35 +0800

    update profile

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,1 +1,41 @@
-Page({})
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  },
+
+  
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    console.log('show')
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  },
+
+  appointment: function(e) {
+    console.log('appointment')
+    wx.navigateTo({
+      url: 'pages/appointment/appointment',
+    })
+  }
+
+})

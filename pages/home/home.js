97d3b9b5ca4864c2576110bab20d32dfbdb38b03
FNhaha <584948862@qq.com>
2019-03-07 20:38:37 +0800

    完善network service

--- pages/home/home.js
+++ pages/home/home.js
@@ -4,16 +4,21 @@
 *7PM,3.5
 */
 var Network=require('../../utils/networkRequestService.js');
+
 Page({
   data: {
     value: '',
 
-    /*
-    *筛选部分，
-    *按顺序从0编号
-    *title：选项名
-    *column：选项列表
-    */
+    campusSelected: all,
+    timeSelected: all,
+    orderSelected: all,
+
+    /**
+     *筛选部分，
+     *按顺序从0编号
+     *title：选项名
+     *column：选项列表
+     */
     title0:'所有校区',
     column0: ['所有校区','鼓楼校区','仙林校区'],
     title1:'时间范围',
@@ -21,41 +26,29 @@ Page({
     title2:'默认排序',
     column2:['默认排序','时间顺序','拼音顺序','拼音倒序'],
     
-    /*
-    resultList为搜索结果
-    */
-    resultList:[{
-      name:"hbm0",
-      freeTime:"今天晚上"
-    },{
-      name:"hbm1",
-      freeTime:"今天上午"
-    },{
-      name:"hbm2",
-      freeTime:"明天上午"
-    }, {
-      name: "hbm3",
-      freeTime: "今天晚上"
-    }, {
-      name: "hbm4",
-      freeTime: "今天晚上"
-    }, {
-      name: "hbm5",
-      freeTime: "今天上午"
-    }, {
-      name: "hbm6",
-      freeTime: "明天上午"
-    }, {
-      name: "hbm7",
-      freeTime: "今天晚上"
-    },]
+    /**
+     resultList为搜索结果
+     */
+    resultList:null,
   },
 
+/**
+ * 生命周期函数--监听页面加载
+ */
+onLoad: function (options) {
+  var that = this;
+  //this.setData({resultList:Network.GetAllTutor(0,10).tutorList.content});
+  Network.GetAllTutor(0,10,function(res){
+      that.setData({resultList:res.data.tutorList});
+      console.log(res);
+  });
+},
 
-/*搜索栏
-*onChange——输入发生变化时调用
-*onSearch——点击搜索时调用
-*/
+/**
+ *搜索栏
+ *onChange——输入发生变化时调用
+ *onSearch——点击搜索时调用
+ */
   onChange(e) {
     this.setData({
       value: e.detail
@@ -64,7 +57,7 @@ Page({
 
   onSearch(event) {
     if (this.data.value) {
-      Network.GetTutorById();
+      resultList:;
       wx.showToast({
         title: '搜索：' + this.data.value,
         icon: 'none'
@@ -75,6 +68,7 @@ Page({
         title: '请输入搜索内容',
         icon:'none'
       })
+
     }
   },
 
@@ -164,5 +158,7 @@ Page({
       title: 'name: '+event.target.dataset.name,
       icon:'none'
     })
-  }
+  },
+
+  
 });
--- utils/networkRequestService.js
+++ utils/networkRequestService.js
@@ -1,144 +1,203 @@
 var url='http://192.168.2.229:8080/';
 
 module.exports={
-  GetAllTutor(pageNum,pageSize) {
+
+  /**
+   * 
+   * @param {*} pageNum 
+   * @param {*} pageSize 
+   * @param {*} successFunction Data: tutorList: {id, wxId, tutorName, tutorDescription, freeTimeList: {id, campus, dayOfWeek, beginTime, endTime}}
+   */
+  GetAllTutor(pageNum,pageSize,successFunction) {
     wx.request({
       url: url+'GetAllTutor',
       method: "POST",
-      header: {
-        'content-type': 'application/json'
-      },
       data: {
         "pageNum": pageNum,
         "pageSize": pageSize
       },
-      success(res) {
-        console.log(res);
-      }
+      success: successFunction
     })
   },
-  GetTutorById(id) {
+
+  /**
+   * 
+   * @param {*} id Data id
+   * @param {*} successFunction tutor: {id, wxId, tutorName, tutorDescription, freeTimeList: {id, campus, dayOfWeek, beginTime, endTime}}
+   */
+  GetTutorById(id,successFunction) {
     wx.request({
       url: url+'GetTutorById',
       method: "GET",
-      header: {
-        'content-type': 'application/json'
-      },
       data: {
         "id": id
       },
-      success(res) {
-        console.log(res);
-      }
+      success: successFunction
     })
   },
-  GetTutorByWxId(WxID) {
+
+  /**
+   * 
+   * @param {*} WxID Data: wxId
+   * @param {*} successFunction Data: tutorList: {id, wxId, tutorName, tutorDescription, freeTimeList: {id, campus, dayOfWeek, beginTime, endTime}}
+   */
+  GetTutorByWxId(WxID,successFunction) {
     wx.request({
       url: url+'GetTutorByWxId',
       method: "GET",
-      header: {
-        'content-type': 'application/json'
-      },
       data: {
         "wxId": WxID
       },
-      success(res) {
-        console.log(res);
-      }
+      success: successFunction
     })
   },
-  UpdateTutorById(Data) {
+
+  /**
+   * 
+   * @param {*} Data Date: id, wxId, tutorName, tutorDescription, freeTimeList: {campus, beginTime, endTime, dayOfWeek}
+   * @param {*} successFunction 
+   */
+  UpdateTutorById(Data,successFunction) {
     wx.request({
       url: url+'UpdateTutorById',
       method: "POST",
-      header: {
-        'content-type': 'application/json'
-      },
       data: Data,
-      success(res) {
-        console.log(res);
-      }
+      success: successFunction
     })
   },
-  GetStudentById(ID) {
+
+  /**
+   * 
+   * @param {*} ID Data: id
+   * @param {*} successFunction Data: id, wxId, studentNumber, studentName, phone, email
+   */
+  GetStudentById(ID,successFunction) {
     wx.request({
       url: url+'GetStudentById',
       method: "GET",
-      header: {
-        'content-type': 'application/json'
-      },
       data: {
         "id": ID
       },
-      success(res) {
-        console.log(res);
-      }
+      success: successFunction
     })
   },
-  GetStudentByWxId(WxID) {
+
+  /**
+   * 
+   * @param {*} WxID Data: wxId
+   * @param {*} successFunction Data: id, wxId, studentNumber, studentName, phone, email
+   */
+  GetStudentByWxId(WxID,successFunction) {
     wx.request({
       url: url+'GetStudentByWxId',
       method: "GET",
-      header: {
-        'content-type': 'application/json'
-      },
       data: {
         "wxId": WxID
       },
-      success(res) {
-        console.log(res);
-      }
+      success: successFunction
     })
   },
-  UpdateStudentById(Data) {
+
+  /**
+   * 
+   * @param {*} Data Data: id, wxId, studentNumber, studentName, phone, email
+   * @param {*} successFunction Data: id
+   */
+  UpdateStudentById(Data,successFunction) {
     wx.request({
       url: url+'UpdateStudentById',
       method: "POST",
-      header: {
-        'content-type': 'application/json'
-      },
-      data: Data
+      data: Data,
+      success: successFunction
     })
   },
-  InsertAppointment(Data) {
+
+  /**
+   * 
+   * @param {*} Data Data: tutorId, studentId, beginTime, endTime, reservatingDate, status
+   * @param {*} successFunction Data: id, tutorId, studentId, beginTime, endTime, reservatingDate, status
+   */
+  InsertAppointment(Data,successFunction) {
     wx.request({
       url: url+'InsertAppointment',
       method: "POST",
-      header: {
-        'content-type': 'application/json'
-      },
       data: Data,
-      success(res) {
-        console.log(res);
-      }
+      success: successFunction
     })
   },
-  SearchAppointmentByTutorId(Data) {
+
+  /**
+   * 
+   * @param {*} Data Data: tutorId, pageNum, pageSize
+   * @param {*} successFunction appointmentList: {id, tutor: {id, tutorName, tutorDescription, freeTimeList: {id, campus, dayOfWeek, beginTime, endTime}, wxId}, student: {id, wxId, studentNumber, studentName, phone, email}, beginTime, endTime, reservatingDate, status} 
+   */
+  SearchAppointmentByTutorId(Data,successFunction) {
     wx.request({
       url: url+'SearchAppointmentByTutorId',
       method: "POST",
-      header: {
-        'content-type': 'application/json'
-      },
       data: Data,
-      success(res) {
-        console.log(res);
-      }
+      success: successFunction
     })
   },
-  SearchActiveAppointmentByTutorId(TutorID) {
+
+  /**
+   * 
+   * @param {*} TutorID Tutor ID
+   * @param {*} successFunction appointmentList: {id, tutor: {id, tutorName, tutorDescription, freeTimeList: {id, campus, dayOfWeek, beginTime, endTime}, wxId}, student: {id, wxId, studentNumber, studentName, phone, email}, beginTime, endTime, reservatingDate, status}
+   */
+  SearchActiveAppointmentByTutorId(TutorID,successFunction) {
     wx.request({
       url: url+'SearchActiveAppointmentByTutorId',
       method: "GET",
-      header: {
-        'content-type': 'application/json'
-      },
       data: {
         "tutorId": TutorID
       },
-      success(res) {
-        console.log(res);
-      }
+      success: successFunction
+    })
+  },
+  
+  /**
+   * 
+   * @param {*} Data Data: nameKeyword, freeTimeList: {campus, date, beginTime, endTime}, pageNum, pageSize
+   * @param {*} successFunction 
+   */
+  SearchTutorByNameAndFreeTime(Data,successFunction){
+    wx.request({
+      url: url+'SearchTutorByNameAndFreeTime',
+      method:"POST",
+      data:Data,
+      success:successFunction
+    })
+  },
+  
+  
+  /**
+   * 
+   * @param {*} StuInfo Data: studentNumber, studentName, phone, email, wxId
+   * @param {*} successFunction checking if it is successful
+   * @returns Data: id, wxId, studentNumber, studentName, phone, email
+   */
+  InsertStudent(StuInfo,successFunction){
+    wx.request({
+      url: url+'InsertStudent',
+      method:"POST",
+      data:StuInfo,
+      success:successFunction
+    })
+  },
+  
+  /**
+   * 
+   * @param {*} Id Appointment ID
+   * @param {*} successFunction 
+   * @returns Data: id, tutor: {id, tutorName, tutorDesciption, freeTimeList: {id, campus, dayOfWeek, beginTime, endTime}, wxId}, sutdent: {id, wxId, studentNumber, phone, email}, beginTime, endTime, reservatingDate, status
+   */
+  GetAppointmentById(Id, successFunction){
+    wx.request({
+      url: url+'GetAppointmentById',
+      method:"GET",
+      data: Id,
+      success:successFunction
     })
   }
 }

def47083cb34ca8d1b7058389ac4ef43d14caa79
FNhaha <584948862@qq.com>
2019-03-11 20:03:18 +0800

    '完成首页基本功能'

--- pages/home/home.js
+++ pages/home/home.js
@@ -10,7 +10,14 @@ Page({
     /**
      * 搜索框输入值
      */
-    value: '',
+    serchBarValue: '',
+
+    currentDate: new Date().getTime(),
+    currentTargetDate: new Date(),
+    beginDate: new Date(),
+    endDate: new Date(),
+    currentbeginDateSelected: new Date().getTime(),
+    
 
     campusSelected: 'all',
     dateSelected: '20190305',
@@ -26,9 +33,9 @@ Page({
      */
     title0:'所有校区',
     column0: ['所有校区','鼓楼校区','仙林校区'],
-    title1:'时间范围',
+    title1:'开始时间',
     column1:['所有时间','今天上午','今天下午','今天晚上','明天上午'],
-    title2:'默认排序',
+    title2:'结束时间',
     column2:['默认排序','时间顺序','拼音顺序','拼音倒序'],
     
     /**
@@ -56,31 +63,19 @@ onLoad: function (options) {
  */
   onChange(e) {
     this.setData({
-      value: e.detail
+      serchBarValue: e.detail
     });
   },
 
   onSearch(event) {
     var that = this;
     //if (this.data.value) {
-      Network.SearchTutorByNameAndFreeTime({
-        "nameKeyword": that.data.value,
-        "freeTimeList": [
-          {
-            "campus": that.data.campusSelected,
-            "date": that.data.dateSelected,
-            "beginTime": that.data.biginTimeSelected,
-            "endTime": that.data.endTimeSelected
-          }
-        ],
-        "pageNum": 0,
-        "pageSize": 5
-      },function(res){
+      this.searchTutorByNameAndFreeTime(function(res){
           that.setData({resultList: res.data.tutorList});
           console.log(res);
       })
       wx.showToast({
-        title: '搜索：' + value,
+        title: '搜索：' + that.data.serchBarValue,
         icon: 'none'
       });
     /*}
@@ -94,14 +89,15 @@ onLoad: function (options) {
   },
 
 
-/*
-* 筛选栏部分
-* 对应编号
-* tap——点击时调用
-* onConfirm——点击确认时调用
-* onChange——改变选项，未点确认时调用
-* onCancel——点击取消时调用
-*/
+/**
+ * 筛选栏部分
+ * 对应编号
+ * tap——点击时调用
+ * onConfirm——点击确认时调用
+ * onChange——改变选项，未点确认时调用
+ * onCancel——点击取消时调用
+ * onDateSelectChange——选择日期改变时调用
+ */
   toggle(type){
     this.setData({
       [type]:!this.data[type]
@@ -147,19 +143,7 @@ onLoad: function (options) {
       campusSelected: campus
     });
 
-    Network.SearchTutorByNameAndFreeTime({
-      "nameKeyword": that.data.value,
-      "freeTimeList": [
-        {
-          "campus": that.data.campusSelected,
-          "date": that.data.dateSelected,
-          "beginTime": that.data.biginTimeSelected,
-          "endTime": that.data.endTimeSelected
-        }
-      ],
-      "pageNum": 0,
-      "pageSize": 5
-    }, function(res){
+    this.searchTutorByNameAndFreeTime(function(res){
       that.setData({resultList: res.data.tutorList});
       console.log(res);
     });
@@ -171,28 +155,38 @@ onLoad: function (options) {
     
   },
 
-  onConfirm1(event){
-    const { value, index } = event.detail;
-    wx.showToast({
-      title: `index: ${index}, value: ${value}`,
-      icon: 'none'
-    });
-    this.setData({ title1: value });
-    this.toggle('picker1');
+  onDateSelectChange(event){
+    const {detail, currentTarget}=event;
+    this.setData({currentTargetDate: new Date(detail)});
+    console.log(this.data.currentTargetDate)
   },
 
-  onChange1(event) {
-
+  onConfirm1(event){
+    var that = this;
+    this.setData({
+      dateSelected: that.getDateInFormat(that.data.currentTargetDate, "yyyyMMdd"),
+      beginTimeSelected: that.getDateInFormat(that.data.currentTargetDate, "hhmm"),
+      currentbeginDateSelected: that.data.currentTargetDate.getTime()
+    });
+    this.searchTutorByNameAndFreeTime(function(res){
+      that.setData({resultList: res.data.tutorList});
+      console.log(res);
+    });
+    console.log(this.data.currentDate);
+    console.log(this.data.currentbeginDateSelected);
+    this.toggle("picker1");
   },
 
   onConfirm2(event) {
-    const { value, index } = event.detail;
-    wx.showToast({
-      title: `index: ${index}, value: ${value}`,
-      icon: 'none'
+    var that = this;
+    this.setData({
+      endTimeSelected: that.getDateInFormat(that.data.currentTargetDate, "hhmm")
     });
-    this.setData({ title2: value });
-    this.toggle('picker2');
+    this.searchTutorByNameAndFreeTime(function(res){
+      that.setData({resultList: res.data.tutorList});
+      console.log(res);
+    });
+    this.toggle("picker2");
   },
 
   onChange2(event) {
@@ -229,5 +223,47 @@ onLoad: function (options) {
     })
   },
 
-  
+
+  /**
+   * 
+   * @param {*} successFunction 
+   */
+  searchTutorByNameAndFreeTime(successFunction){
+    var that = this;
+    Network.SearchTutorByNameAndFreeTime({
+      "nameKeyword": that.data.serchBarValue,
+      "freeTimeList": [
+        {
+          "campus": that.data.campusSelected,
+          "date": that.data.dateSelected,
+          "beginTime": that.data.biginTimeSelected,
+          "endTime": that.data.endTimeSelected
+        }
+      ],
+      "pageNum": 0,
+      "pageSize": 5
+    }, successFunction)
+  },
+
+  getDateInFormat(date, fmt){
+    var o = {
+      "M+": date.getMonth()+1,
+      "d+": date.getDate(),
+      "h+": date.getHours(),
+      "m+": date.getMinutes(),
+      "s+": date.getSeconds(),
+      "S": date.getMilliseconds(),
+      "q+": Math.floor(date.getMonth()/3+1)
+    }
+    if(/(y+)/.test(fmt)){
+      fmt=fmt.replace(RegExp.$1, (date.getFullYear()+"").substr(4-RegExp.$1.length));
+    }  
+    for(var k in o){
+      if(new RegExp("("+k+")").test(fmt)){
+        fmt=fmt.replace(RegExp.$1, (RegExp.$1.length==1)?(o[k]):(("00"+o[k]).substr((""+o[k]).length)));
+      }
+    }
+    console.log(fmt);
+    return fmt;
+  }
 });
